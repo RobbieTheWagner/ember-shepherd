ac7b85ddb86bb3c2e1739158d93de77876f4699b
Robert Wagner <rwwagner@Roberts-MacBook-Pro.local>
2015-03-18 10:14:12 -0400

    Trying to deploy to gh-pages

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,9 @@
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment, appConfig) {
+  var ENV = {};
+  if (environment === 'production') {
+    ENV.baseURL = '/name-of-your-repo'
+  }
+  return ENV;
 };
