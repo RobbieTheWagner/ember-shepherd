08b822b0aa4cc9f872769d06ec09cfce046cf1a0
Robert Wagner <rwwagner@Roberts-MacBook-Pro.local>
2015-03-19 18:05:20 -0400

    Added blueprint to make bower install work

---
+++ blueprints/ember-shepherd/index.js
@@ -0,0 +1,8 @@
+//blueprints/ember-shepherd/index.js
+module.exports = {
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
+
+  afterInstall: function() {
+    return this.addBowerPackageToProject('shepherd.js'); // is a promise
+  }
+};
