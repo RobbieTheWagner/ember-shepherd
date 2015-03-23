5de4897d96b0bb22cd9a8a8cfde45ab029f1f2e2
Robert Wagner <rwwagner@Roberts-MacBook-Pro.local>
2015-03-23 10:51:27 -0400

    Added broken test

--- tests/integration/ember-shepherd-test.js
+++ tests/integration/ember-shepherd-test.js
@@ -6,7 +6,7 @@ import startApp from '../helpers/start-app';
 import '../helpers/controller';
 var App;
 
-module('An Integration test', {
+module('Tour functionality tests', {
   beforeEach: function() {
     App = startApp();
   },
@@ -21,9 +21,9 @@ module('An Integration test', {
 test("Modal page contents", function(assert) {
   assert.expect(3);
   visit('/').then(function() {
-    assert.equal($('body.shepherd-active').length, 1, "Body gets class of shepherd-active, when shepherd becomes active");
-    assert.equal($('.shepherd-enabled').length, 2, "attachTo element and tour have shepherd-enabled class");
-    assert.equal($('#shepherdOverlay').length, 1, "#shepherdOverlay exists, since modal");
+    assert.equal(find('body.shepherd-active', 'html').length, 1, "Body gets class of shepherd-active, when shepherd becomes active");
+    assert.equal(find('.shepherd-enabled', 'body').length, 2, "attachTo element and tour have shepherd-enabled class");
+    assert.equal(find('#shepherdOverlay', 'body').length, 1, "#shepherdOverlay exists, since modal");
   });
 });
 
@@ -32,25 +32,25 @@ test("Non-modal page contents", function(assert) {
   appController.set('isModal', false);
   assert.expect(3);
   visit('/').then(function() {
-    assert.equal($('body.shepherd-active').length, 1, "Body gets class of shepherd-active, when shepherd becomes active");
-    assert.equal($('.shepherd-enabled').length, 2, "attachTo element and tour get shepherd-enabled class");
-    assert.equal($('#shepherdOverlay').length, 0, "#shepherdOverlay should not exist, since non-modal");
+    assert.equal(find('body.shepherd-active', 'html').length, 1, "Body gets class of shepherd-active, when shepherd becomes active");
+    assert.equal(find('.shepherd-enabled', 'body').length, 2, "attachTo element and tour get shepherd-enabled class");
+    assert.equal(find('#shepherdOverlay', 'body').length, 0, "#shepherdOverlay should not exist, since non-modal");
   });
 });
 
-/*
+
 test("Tour next, back, and cancel builtInButtons work", function(assert) {
   assert.expect(6);
   visit('/').then(function() {
-    assert.equal($('body.shepherd-active').length, 1, "Body gets class of shepherd-active, when shepherd becomes active");
-    assert.equal($('.shepherd-enabled').length, 2, "attachTo element and tour get shepherd-enabled class");
-    assert.equal($('#shepherdOverlay').length, 1, "#shepherdOverlay exists");
-    $("a:contains('Next'):visible")[0].click();
-    assert.equal($("a:contains('Back'):visible").length, 1, "Ensure that the back button appears");
-    $("a:contains('Back'):visible")[0].click();
-    assert.equal($("a:contains('Back'):visible").length, 0, "Ensure that the back button disappears");
-    $("a:contains('Exit'):visible")[0].click();
+    assert.equal(find('body.shepherd-active', 'html').length, 1, "Body gets class of shepherd-active, when shepherd becomes active");
+    assert.equal(find('.shepherd-enabled', 'body').length, 2, "attachTo element and tour have shepherd-enabled class");
+    assert.equal(find('#shepherdOverlay', 'body').length, 1, "#shepherdOverlay exists, since modal");
+    $('.shepherd-enabled .next-button')[0].click();
+    assert.equal(find('.back-button', '.shepherd-enabled').length, 1, "Ensure that the back button appears");
+    $('.shepherd-enabled .back-button')[0].click();
+    assert.equal($('.back-button', '.shepherd-enabled').length, 0, "Ensure that the back button disappears");
+    $('.shepherd-enabled .cancel-button')[0].click();
     assert.equal($('[class^=shepherd-button]:visible').length, 0, "Ensure that all buttons are gone, after exit");
   });
 });
-*/
+
