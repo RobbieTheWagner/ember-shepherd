d49446ad331094837ef6f5ed8520ec247453477e
Robert Wagner <rwwagner@Roberts-MBP.home>
2015-03-31 13:38:53 -0400

    Changed show to before-show

--- addon/components/ember-shepherd.js
+++ addon/components/ember-shepherd.js
@@ -25,7 +25,7 @@ export default Ember.Component.extend({
             tour.addStep(step.id, shepherdStepOptions);
 
             var currentStep = tour.steps[index];
-            currentStep.on('show', function() {
+            currentStep.on('before-show', function() {
               if (this.get('modal')) {
                 $(currentStep.options.attachTo.split(' ')[0])[0].style.pointerEvents = 'none';
                 if (currentStep.options.copyStyles) {
