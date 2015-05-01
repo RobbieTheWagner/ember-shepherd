6d8e1c00218dde408350172d04514214e13ec305
Alex LaFroscia <alex@lafroscia.com>
2015-04-30 23:37:38 -0400

    Update ember-cli-blanket

--- tests/blanket-options.js
+++ tests/blanket-options.js
@@ -1,9 +1,24 @@
-/* globals blanket */
+/* globals blanket, module */
 
-blanket.options({
+var options = {
    modulePrefix: "dummy",
    filter: "//.*dummy/.*/",
    antifilter: "//.*(tests|template).*/",
    loaderExclusions: [],
-   enableCoverage: true
-});
+   enableCoverage: true,
+   cliOptions: {
+     jsonOptions: {
+       outputFile: 'test-output.json'
+     },
+     lcovOptions: {
+       outputFile: 'lcov.dat'
+     },
+     reporters: ['json']
+   }
+};
+
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
