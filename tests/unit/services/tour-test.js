3cd5459d3d9c3edc0f53330ab59c651b9d847839
Alex LaFroscia <alex@lafroscia.com>
2015-05-10 13:14:42 -0400

    Cover remaining component tests with service tests
    The tests are skipped for now because I'm still figuring out how to make them work, since they require some DOM to search through and I don't know if/how that is available from the regular unit tests

--- tests/unit/services/tour-test.js
+++ tests/unit/services/tour-test.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
+import { skip } from 'qunit';
 
 var steps = [
   {
@@ -55,5 +56,31 @@ test('it starts the tour when the `start` event is triggered', function(assert) 
   Ember.run(function() {
     service.trigger('start');
   });
+
+});
+
+skip('attachTo works with object when element is string', function(assert) {
+  assert.expect(1);
+
+  // Render some DOM do create an element to find
+  Ember.run(function() {
+    const view = Ember.View.create({
+      template: Ember.Handlebars.compile('<div class=".test-object">test</div>')
+    });
+    view.appendTo('#qunit-fixture');
+  });
+
+  const service = this.subject({});
+  const step = {
+    options: {
+      attachTo: {
+        element: '.text-object'
+      }
+    }
+  };
+  const element = service._getElementForStep(step);
+});
+
+skip('attachTo works with object when element is DOM object', function(assert) {
 
 });
