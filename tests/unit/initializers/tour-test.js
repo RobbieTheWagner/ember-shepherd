df613f33a7bff438e648f8d4e30f700eb3b22e17
Alex LaFroscia <alex@lafroscia.com>
2015-05-10 12:07:40 -0400

    Add test for initializer

--- tests/unit/initializers/tour-test.js
+++ tests/unit/initializers/tour-test.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
-import { initialize } from '../../../initializers/tour';
+// See comment below about Ember CLI bug
+// This should be fixed with the next CLI release
+import initializer from '../../../initializers/tour';
 import { module, test } from 'qunit';
 
-var container, application;
+var container, application, mockTourService, mockApplicationController;
 
 module('TourInitializer', {
   beforeEach: function() {
@@ -10,14 +12,21 @@ module('TourInitializer', {
       application = Ember.Application.create();
       container = application.__container__;
       application.deferReadiness();
+
+      mockTourService = Ember.Object.create({});
+      mockApplicationController = Ember.Object.create({});
+
+      application.register('service:tour', mockTourService, {instantiate: false});
+      application.register('controller:application', mockApplicationController, {instantiate: false});
     });
   }
 });
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  initialize(container, application);
-
-  // you would normally confirm the results of the initializer here
-  assert.ok(true);
+test('it injects the application controller into the tour service', function(assert) {
+  // NOTE: this is needed due to a bug in the Ember CLI testing of
+  // addon-provided initializers.  See link for more details
+  // https://github.com/ember-cli/ember-cli/pull/3891
+  initializer.initialize(container, application);
+  assert.equal(mockTourService.get('_applicationController'), mockApplicationController, 'Initializer injects the application controller');
 });
