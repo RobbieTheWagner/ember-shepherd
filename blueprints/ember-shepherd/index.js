31c00ebccb5ad0e77b47b34423d4f366914e1a29
Robert Wagner <rwwagner90@gmail.com>
2015-05-22 13:01:08 -0400

    I think specifying this version here should fix some issues

--- blueprints/ember-shepherd/index.js
+++ blueprints/ember-shepherd/index.js
@@ -3,6 +3,6 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('shepherd.js'); // is a promise
+    return this.addBowerPackageToProject('shepherd.js', '0.7.1'); // is a promise
   }
 };
