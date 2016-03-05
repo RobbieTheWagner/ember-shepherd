1e8e545fd86fc7f09a5b7460468d0892ded00050
Robert Wagner <rwwagner@vt.edu>
2016-03-05 08:51:00 -0500

    Chrome again

--- testem.js
+++ testem.js
@@ -4,9 +4,9 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed&coverage",
   "disable_watching": true,
   "launch_in_ci": [
-    "Firefox"
+    "Chrome"
   ],
   "launch_in_dev": [
-    "Firefox"
+    "Chrome"
   ]
 };
