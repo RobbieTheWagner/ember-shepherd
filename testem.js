af1940ed47dfc2240b40fc65f26a990b1982d57f
Robert Wagner <rwwagner90@gmail.com>
2016-05-09 12:17:03 -0400

    Switch to FF

--- testem.js
+++ testem.js
@@ -4,9 +4,9 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed&coverage",
   "disable_watching": true,
   "launch_in_ci": [
-    "Chrome"
+    "Firefox"
   ],
   "launch_in_dev": [
-    "Chrome"
+    "Firefox"
   ]
 };
