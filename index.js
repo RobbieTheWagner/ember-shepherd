156c84b4d6ddcb33168aeb86cd02d697ca8830b8
Josh Fester <joshfester@gmail.com>
2016-08-15 18:17:11 -0500

    trying to support fastboot

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ module.exports = {
     this.app.import('bower_components/tether-shepherd/dist/css/shepherd-theme-arrows.css');
     this.app.import('bower_components/tether/dist/js/tether.js');
     this.app.import('bower_components/tether-shepherd/dist/js/shepherd.js');
-    this.app.import('vendor/jquery-disablescroll/jquery.disablescroll.js');
+
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      this.app.import('vendor/jquery-disablescroll/jquery.disablescroll.js');
+    }    
   }
 };
