f607bc04776578a5c83957415b2e231ce900365e
Robert Wagner <rwwagner@vt.edu>
2016-09-18 10:32:11 -0400

    Assign polyfill, use Chrome

--- testem.js
+++ testem.js
@@ -1,12 +1,12 @@
-/*jshint node:true*/
+/* eslint-disable */
 module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed&coverage",
   "disable_watching": true,
   "launch_in_ci": [
-    "Firefox"
+    "Chrome"
   ],
   "launch_in_dev": [
-    "Firefox"
+    "Chrome"
   ]
 };
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
-const { assign, run } = Ember;
+import assign from 'ember-assign-polyfill';
+const { run } = Ember;
 
 export default function startApp(attrs) {
   let application;
