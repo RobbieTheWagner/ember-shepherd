285bc574735afad3bffdc5a3efc28465f91e547c
Robert Wagner <rwwagner@vt.edu>
2016-10-19 20:38:02 -0400

    Update CLI

--- blueprints/ember-shepherd/index.js
+++ blueprints/ember-shepherd/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('tether-shepherd', '1.8.0');
+    return this.addBowerPackageToProject('tether-shepherd', '1.8.1');
   }
 };
