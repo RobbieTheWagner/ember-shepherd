4e15ed0b32d90880b03e24c03ce6f40b2433456e
Robert Wagner <rwwagner90@gmail.com>
2018-10-12 13:38:48 -0400

    Move some utils around, add comments, _ private methods (#259)
    * Moved some utils around
    
    * Remove some duplication, add some comments, _ private methods

--- addon/services/tour.js
+++ addon/services/tour.js
@@ -3,11 +3,14 @@ import { get, observer, set } from '@ember/object';
 import { isEmpty, isPresent } from '@ember/utils';
 import Service from '@ember/service';
 import Evented from '@ember/object/evented';
-import { run, scheduleOnce } from '@ember/runloop';
+import { bind, debounce, later } from '@ember/runloop';
+import { normalizeAttachTo } from '../utils/attachTo';
+import { makeButton } from '../utils/buttons';
+import { cleanupModal, cleanupSteps, cleanupStepEventListeners } from '../utils/cleanup';
 import {
   elementIsHidden,
   getElementForStep,
-  toggleShepherdModalClass,
+  toggleShepherdModalClass
 } from '../utils/dom';
 
 import {
@@ -15,7 +18,7 @@ import {
   createModalOverlay,
   positionModalOpening,
   closeModalOpening,
-  classNames as modalClassNames,
+  classNames as modalClassNames
 } from '../utils/modal';
 
 
@@ -36,7 +39,7 @@ export default Service.extend(Evented, {
   _onScreenChange() {},
 
   willDestroy() {
-    this.cleanup();
+    this._cleanup();
   },
 
   /**
@@ -74,14 +77,21 @@ export default Service.extend(Evented, {
     get(this, 'tourObject').show(id);
   },
 
+  /**
+   * Start the tour
+   * @public
+   */
   start() {
     set(this, 'isActive', true);
     get(this, 'tourObject').start();
   },
 
+  /**
+   * When the tour starts, setup the modal overlay, step event listeners, and disableScroll
+   */
   onTourStart() {
-    this.initModalOverlay();
-    this.addStepEventListeners();
+    this._initModalOverlay();
+    this._addStepEventListeners();
 
     if (get(this, 'disableScroll')) {
       disableScroll.on(window);
@@ -98,7 +108,7 @@ export default Service.extend(Evented, {
     if (!this.isDestroyed) {
       set(this, 'isActive', false);
     }
-    this.cleanup();
+    this._cleanup();
     this.trigger(completeOrCancel);
   },
 
@@ -106,17 +116,21 @@ export default Service.extend(Evented, {
    * Cleanup the modal leftovers, like the overlay and highlight, so they don't hang around.
    * @private
    */
-  cleanup() {
+  _cleanup() {
     if (get(this, 'disableScroll')) {
       disableScroll.off(window);
     }
 
-    this.cleanupStepEventListeners();
-    this.cleanupSteps();
-    this.cleanupModal();
+    cleanupStepEventListeners.bind(this)();
+    cleanupSteps(this.tourObject);
+    cleanupModal.bind(this)();
   },
 
-  initialize() {
+  /**
+   * Initializes the tour, creates a new Shepherd.Tour. sets options, and binds events
+   * @private
+   */
+  _initialize() {
     const confirmCancel = get(this, 'confirmCancel');
     const confirmCancelMessage = get(this, 'confirmCancelMessage');
     const defaultStepOptions = get(this, 'defaultStepOptions');
@@ -127,68 +141,26 @@ export default Service.extend(Evented, {
       defaultStepOptions
     });
 
-    tourObject.on('start', run.bind(this, 'onTourStart'));
-    tourObject.on('complete', run.bind(this, 'onTourFinish', 'complete'));
-    tourObject.on('cancel', run.bind(this, 'onTourFinish', 'cancel'));
+    tourObject.on('start', bind(this, 'onTourStart'));
+    tourObject.on('complete', bind(this, 'onTourFinish', 'complete'));
+    tourObject.on('cancel', bind(this, 'onTourFinish', 'cancel'));
 
     set(this, 'tourObject', tourObject);
-    this.initModalOverlay();
+    this._initModalOverlay();
   },
 
   /**
-   * Creates a button of the specified type, with the given classes and text
-   *
-   * @param button.type The type of button cancel, back, or next
-   * @param button.classes Classes to apply to the button
-   * @param button.text The text for the button
-   * @param button.action The action to call
-   * @returns {{action: *, classes: *, text: *}}
+   * If modal is enabled, setup the svg mask opening and modal overlay for the step
+   * @param step
    * @private
    */
-  makeButton(button) {
-    const { type, classes, text } = button;
-
-    if (!type) {
-      return button;
-    }
-
-    const builtInButtonTypes = ['back', 'cancel', 'next'];
-    if (builtInButtonTypes.includes(type)) {
-      const action = run.bind(this, function() {
-        this[type]();
-      });
-
-      return {
-        action,
-        classes,
-        text
-      };
-    }
-  },
-
-  /**
-   * Check if attachTo is an object, if it is, put element and on into a string,
-   * if it is already a string, just return that string
-   *
-   * @param attachTo
-   * @returns {*}
-   * @private
-   */
-  normalizeAttachTo(attachTo) {
-    if (attachTo && typeof attachTo.element === 'string' && typeof attachTo.on === 'string') {
-      return `${attachTo.element} ${attachTo.on}`;
-    } else {
-      return attachTo;
-    }
-  },
-
-  setupModalForStep(step) {
+  _setupModalForStep(step) {
     if (!this.modal) {
-      this.hideModal();
+      this._showOrHideModal('hide');
 
     } else {
-      this.styleModalOpeningForStep(step);
-      this.showModal();
+      this._styleModalOpeningForStep(step);
+      this._showOrHideModal('show');
     }
   },
 
@@ -199,7 +171,7 @@ export default Service.extend(Evented, {
    * @param step The step object that attaches to the element
    * @private
    */
-  styleTargetElementForStep(step) {
+  _styleTargetElementForStep(step) {
     const targetElement = getElementForStep(step);
 
     if (!targetElement) {
@@ -248,7 +220,7 @@ export default Service.extend(Evented, {
    * @private
    */
   stepsChange: observer('steps', function() {
-    this.initialize();
+    this._initialize();
     const steps = get(this, 'steps');
     const tour = get(this, 'tourObject');
 
@@ -273,17 +245,17 @@ export default Service.extend(Evented, {
       const { id, options } = step;
 
       if (options.buttons) {
-        options.buttons = options.buttons.map(this.makeButton, this);
+        options.buttons = options.buttons.map(makeButton.bind(this), this);
       }
-      options.attachTo = this.normalizeAttachTo(options.attachTo);
+      options.attachTo = normalizeAttachTo(options.attachTo);
       tour.addStep(id, options);
 
       // Step up events for the current step
       const currentStep = tour.steps[index];
 
       currentStep.on('before-show', () => {
-        this.setupModalForStep(currentStep);
-        this.styleTargetElementForStep(currentStep);
+        this._setupModalForStep(currentStep);
+        this._styleTargetElementForStep(currentStep);
       });
 
       currentStep.on('hide', () => {
@@ -295,7 +267,7 @@ export default Service.extend(Evented, {
             targetElement.classList.remove(currentStep.options.highlightClass);
           }
 
-          this.hideModal();
+          this._showOrHideModal('hide');
         }
       });
 
@@ -308,7 +280,7 @@ export default Service.extend(Evented, {
             elem.scrollIntoView();
           }
 
-          run.later(() => {
+          later(() => {
             if (get(this, 'disableScroll')) {
               disableScroll.on(window);
             }
@@ -319,18 +291,18 @@ export default Service.extend(Evented, {
     });
   }),
 
-  initModalOverlay() {
+  _initModalOverlay() {
     if (!this._modalOverlayElem) {
       this._modalOverlayElem = createModalOverlay();
       this._modalOverlayOpening = getModalMaskOpening(this._modalOverlayElem);
 
-      this.hideModal();
+      this._showOrHideModal('hide');
 
       document.body.appendChild(this._modalOverlayElem);
     }
   },
 
-  styleModalOpeningForStep(step) {
+  _styleModalOpeningForStep(step) {
     const modalOverlayOpening = this._modalOverlayOpening;
     const targetElement = getElementForStep(step);
 
@@ -338,38 +310,45 @@ export default Service.extend(Evented, {
       positionModalOpening(targetElement, modalOverlayOpening);
 
       this._onScreenChange = () => {
-        run.debounce(
+        debounce(
           this,
-          () => { positionModalOpening(targetElement, modalOverlayOpening) },
+          () => { positionModalOpening(targetElement, modalOverlayOpening); },
           50
         );
       };
 
-      this.addStepEventListeners();
+      this._addStepEventListeners();
 
     } else {
       closeModalOpening(this._modalOverlayOpening);
     }
   },
 
-  showModal() {
-    document.body.classList.add(modalClassNames.isVisible);
+  /**
+   * Show or hide the modal
+   * @param {string} showOrHide 'show' or 'hide'
+   * @private
+   */
+  _showOrHideModal(showOrHide) {
+    const show = showOrHide === 'show';
+
+    if (show) {
+      document.body.classList.add(modalClassNames.isVisible);
+    } else {
+      document.body.classList.remove(modalClassNames.isVisible);
+    }
+
 
     if (this._modalOverlayElem) {
-      this._modalOverlayElem.style.display = 'block';
+      this._modalOverlayElem.style.display = show ? 'block' : 'none';
     }
   },
 
-  hideModal() {
-    document.body.classList.remove(modalClassNames.isVisible);
-
-    if (this._modalOverlayElem) {
-      this._modalOverlayElem.style.display = 'none';
-    }
-
-  },
-
-  addStepEventListeners() {
+  /**
+   * Add resize and scroll listeners to window
+   * @private
+   */
+  _addStepEventListeners() {
     if (typeof this._onScreenChange === 'function') {
       window.removeEventListener('resize', this._onScreenChange, false);
       window.removeEventListener('scroll', this._onScreenChange, false);
@@ -377,46 +356,5 @@ export default Service.extend(Evented, {
 
     window.addEventListener('resize', this._onScreenChange, false);
     window.addEventListener('scroll', this._onScreenChange, false);
-  },
-
-
-  cleanupStepEventListeners() {
-    if (typeof this._onScreenChange === 'function') {
-      window.removeEventListener('resize', this._onScreenChange, false);
-      window.removeEventListener('scroll', this._onScreenChange, false);
-
-      this._onScreenChange = null;
-    }
-  },
-
-  cleanupSteps() {
-    const tour = this.tourObject;
-
-    if (tour) {
-      const { steps } = tour;
-
-      steps.forEach((step) => {
-        if (step.options && step.options.canClickTarget === false && step.options.attachTo) {
-          const stepElement = getElementForStep(step);
-
-          if (stepElement instanceof HTMLElement) {
-            stepElement.style.pointerEvents = 'auto';
-          }
-        }
-      });
-    }
-  },
-
-  cleanupModal() {
-    scheduleOnce('afterRender', this, () => {
-      const element = this._modalOverlayElem;
-
-      if (element && element instanceof SVGElement) {
-        element.parentNode.removeChild(element);
-      }
-
-      this._modalOverlayElem = null;
-      document.body.classList.remove(modalClassNames.isVisible);
-    });
-  },
+  }
 });
