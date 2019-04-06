c41e2a3fdfaf0a05fba91c50698d5eb97a948103
Robert Wagner <rwwagner90@gmail.com>
2019-04-06 16:55:19 -0400

    Convert example styles to tailwind

--- tests/dummy/app/controllers/docs/demo.js
+++ tests/dummy/app/controllers/docs/demo.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { steps as defaultSteps } from '../data';
+import { steps as defaultSteps } from '../../data';
 
 export default Controller.extend({
   tour: service(),
