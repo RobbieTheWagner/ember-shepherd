7d64c4b29ba53b6e225f4ef78164421089c5811c
Robert Wagner <rwwagner90@gmail.com>
2020-02-22 07:52:15 -0500

    Fix template lint bug

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,6 +3,8 @@
 module.exports = {
   extends: 'octane',
   rules: {
-    'no-curly-component-invocation': { allow: ['svg-jar'] }
+    'no-curly-component-invocation': { allow: ['svg-jar'] },
+    // TODO turn this back on when fixed https://github.com/ember-template-lint/ember-template-lint/issues/1135
+    'no-quoteless-attributes': false
   }
 };
