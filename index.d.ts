0f9a086729adf41fad172b68e6633a8c92a2db72
Charles Fries <charliefries@icloud.com>
2022-12-08 08:54:48 -0800

    Add ambient types

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+declare module 'ember-shepherd/services/tour' {
+  import type Shepherd from 'shepherd.js';
+
+  export interface TourButton extends Shepherd.Step.StepOptionsButton {
+    type: 'next' | 'back' | 'cancel';
+  }
+
+  interface RequiredElement {
+    selector: string;
+    message: string;
+    title: string;
+  }
+
+  export default interface Tour {
+    classPrefix: string;
+    confirmCancel: boolean;
+    confirmCancelMessage: string | null;
+    defaultStepOptions: Record<string, unknown>;
+    errorTitle: string | null;
+    exitOnEsc: boolean;
+    isActive: boolean;
+    keyboardNavigation: boolean;
+    messageForUser: string | null;
+    modal: boolean;
+    modalContainer: HTMLElement;
+    requiredElements: RequiredElement[];
+    steps: Shepherd.Step[];
+    addSteps: (steps: Shepherd.Step.StepOptions[]) => void;
+    back: () => void;
+    cancel: () => void;
+    complete: () => void;
+    hide: () => void;
+    next: () => void;
+    show: (id: string) => void;
+    start: () => void;
+    _onTourStart: () => void;
+    _onTourFinish: (completeOrCancel: string) => void;
+    _initialize: () => void;
+    _requiredElementsPresent: () => boolean;
+  }
+}
