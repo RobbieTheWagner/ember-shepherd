32073b0ca97747220d8c5a307c0adc07f453b044
Robbie Wagner <rwwagner90@gmail.com>
2024-11-13 14:22:34 -0500

    Reset app and router

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,39 +1,20 @@
-function _defineProperty(e, r, t) {
-  return (
-    (r = _toPropertyKey(r)) in e
-      ? Object.defineProperty(e, r, {
-          value: t,
-          enumerable: !0,
-          configurable: !0,
-          writable: !0,
-        })
-      : (e[r] = t),
-    e
-  );
-}
-function _toPropertyKey(t) {
-  var i = _toPrimitive(t, 'string');
-  return 'symbol' == typeof i ? i : i + '';
-}
-function _toPrimitive(t, r) {
-  if ('object' != typeof t || !t) return t;
-  var e = t[Symbol.toPrimitive];
-  if (void 0 !== e) {
-    var i = e.call(t, r || 'default');
-    if ('object' != typeof i) return i;
-    throw new TypeError('@@toPrimitive must return a primitive value.');
-  }
-  return ('string' === r ? String : Number)(t);
-}
-import EmberRouter from '@ember/routing/router';
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 import config from 'dummy/config/environment';
 
-export default class Router extends EmberRouter {
-  constructor(...args) {
-    super(...args);
-    _defineProperty(this, 'location', config.locationType);
-    _defineProperty(this, 'rootURL', config.rootURL);
-  }
-}
+const Router = AddonDocsRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+});
 
-Router.map(function () {});
+Router.map(function () {
+  docsRoute(this, function () {
+    this.route('demo');
+    this.route('faq');
+    this.route('style-variables');
+    this.route('usage');
+
+    this.route('not-found', { path: '/*path' });
+  });
+});
+
+export default Router;
