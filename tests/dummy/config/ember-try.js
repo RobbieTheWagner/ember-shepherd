464084aeb7c1eeba9670058a52d16db249ea80d7
Robbie Wagner <rwwagner90@gmail.com>
2024-11-13 14:55:26 -0500

    Switch to pnpm (#1701)
    * Switch to pnpm
    
    * Update ember-cli-update.json
    
    * ember-cli-update
    
    * Update ci.yml
    
    * Reset app and router
    
    * Update ember-cli-build.js
    
    * Delete resolver.js

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -5,21 +5,21 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
-    useYarn: true,
+    usePnpm: true,
     scenarios: [
-      {
-        name: 'ember-lts-4.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-          },
-        },
-      },
       {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
             'ember-source': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.4.0',
           },
         },
       },
