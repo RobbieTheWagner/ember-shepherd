cd0d50d27e0451d431a904fa077fc5690416580f
Robbie Wagner <rwwagner90@gmail.com>
2024-11-19 10:37:53 -0500

    Fix lint

--- test-app/.stylelintrc.js
+++ test-app/.stylelintrc.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'color-hex-length': null,
+    'font-family-no-missing-generic-family-keyword': null,
+  },
 };
