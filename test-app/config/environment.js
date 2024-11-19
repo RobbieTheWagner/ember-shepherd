b2349894a11cf0867109aa4e9c3d6d6f55be744a
Robbie Wagner <rwwagner90@gmail.com>
2024-11-19 12:54:32 -0500

    Update environment.js

--- test-app/config/environment.js
+++ test-app/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // Allow ember-cli-addon-docs to update the rootURL in compiled assets
-    ENV.rootURL = 'ADDON_DOCS_ROOT_URL';
+    ENV.rootURL = 'https://RobbieTheWagner.github.io/ember-shepherd/';
   }
 
   return ENV;
