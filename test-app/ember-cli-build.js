a518c5c04d1fcd6e9a50057366b720da2ac0089b
Robbie Wagner <rwwagner90@gmail.com>
2024-11-19 13:38:59 -0500

    Go back to not full embroider

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const { Webpack } = require('@embroider/webpack');
+// const { Webpack } = require('@embroider/webpack');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
@@ -13,31 +13,35 @@ module.exports = function (defaults) {
     },
   });
 
-  return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTrees: true,
-    staticAddonTestSupportTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
-    staticEmberSource: true,
+  const { maybeEmbroider } = require('@embroider/test-setup');
 
-    packagerOptions: {
-      webpackConfig: {
-        // slow, but highest fidelity
-        devtool: 'source-map',
-        module: {
-          rules: [
-            {
-              test: /\.(png|svg|jpg|jpeg|gif)$/i,
-              type: 'asset/resource',
-            },
-            {
-              test: /\.(woff|woff2|eot|ttf|otf)$/i,
-              type: 'asset/resource',
-            },
-          ],
-        },
-      },
-    },
-  });
+  return maybeEmbroider(app);
+
+  // return require('@embroider/compat').compatBuild(app, Webpack, {
+  //   staticAddonTrees: true,
+  //   staticAddonTestSupportTrees: true,
+  //   staticHelpers: true,
+  //   staticModifiers: true,
+  //   staticComponents: true,
+  //   staticEmberSource: true,
+
+  //   packagerOptions: {
+  //     webpackConfig: {
+  //       // slow, but highest fidelity
+  //       devtool: 'source-map',
+  //       module: {
+  //         rules: [
+  //           {
+  //             test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  //             type: 'asset/resource',
+  //           },
+  //           {
+  //             test: /\.(woff|woff2|eot|ttf|otf)$/i,
+  //             type: 'asset/resource',
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   },
+  // });
 };
