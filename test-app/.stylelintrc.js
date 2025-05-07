6f293b66402a5f4b9716d6c52e9d2b08e51ec130
Robbie Wagner <robbie.wagner@hashicorp.com>
2025-05-07 09:35:38 -0400

    Update .stylelintrc.js

--- test-app/.stylelintrc.js
+++ test-app/.stylelintrc.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  extends: ['stylelint-config-standard'],
   rules: {
     'color-hex-length': null,
     'font-family-no-missing-generic-family-keyword': null,
