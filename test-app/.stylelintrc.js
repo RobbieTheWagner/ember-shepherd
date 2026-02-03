dec9d2e547e7e83725fdd6faf30780b36999abd8
Robbie Wagner <robbie.wagner@hashicorp.com>
2026-02-03 12:07:56 -0500

    Update to latest blueprints

--- test-app/.stylelintrc.js
+++ test-app/.stylelintrc.js
@@ -1,9 +1,5 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
-  rules: {
-    'color-hex-length': null,
-    'font-family-no-missing-generic-family-keyword': null,
-  },
+  extends: ['stylelint-config-standard'],
 };
